956ca442e163c470d76cc601e7014892989dc788
Alysson69 <48935508+Alysson69@users.noreply.github.com>
2019-10-16 10:46:52 -0300

    Create Origem.cpp

---
+++ Origem.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include<string>
+#include<iostream>
+
+//using namespace std;
+
+#include "carro.h"
+
+int main()
+{
+		Carro c1;
+		c1.setAno (2018);
+		c1.setMarca ("ford");
+		c1.setCor ("cinza");
+
+		Carro c2;
+
+		c2.setAno (2012);
+		c2.setMarca ("ferrari");
+
+		Carro c3;
+		c3.setAno  (2015);
+		c3.setMarca  ("fiat");
+		c3.setCor  ("cinza");
+
+		Carro c4 ("jeep", 2005);
+		
+		c4.setCor ("cinza");
+		
+
+			Carro c5;
+		c5.setAno  (2017);
+		c5.setMarca  ("fiat");
+		c5.setCor  ("cinza");
+		c5.pneus = 2;			//atributo da classe
+
+		Carro c6;
+		c6.setAno (2015);
+		c6.setCor ("preto");
+		c6.setMarca ("ford");
+
+		cout << c1.getMarca() << endl;
+		cout << c1.getCor() << endl;
+		cout << c1.getAno() << endl;
+		cout << c1.pneus << endl;
+		cout << c2.getMarca() << endl;
+		cout << c2.getCor() << endl;
+		cout << c2.getAno() << endl;
+		cout << c2.pneus << endl;
+		cout << c3.getMarca() << endl;
+		cout << c3.getCor() << endl;
+		cout << c3.getAno() << endl;
+		cout << c3.pneus << endl;
+		cout << c4.getMarca() << endl;
+		cout << c4.getCor() << endl;
+		cout << c4.getAno() << endl;
+		cout << c5.getMarca() << endl;
+		cout << c5.getCor() << endl;
+		cout << c5.getAno() << endl;
+		cout << c6.getMarca() << endl;
+		cout << c6.getCor() << endl;
+		cout << c6.getAno() << endl;
+
+		return 0;
+}
